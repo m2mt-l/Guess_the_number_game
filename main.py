112fead149769a27d3084508155f0034c093608d
m2mt-l <ta28ma2@gmail.com>
2022-12-30 23:46:08 +0000

    feat: add check range

--- main.py
+++ main.py
@@ -4,24 +4,28 @@ ATTEMPT_NUMBER_HASH = {1: "1st", 2: "2nd", 3: "3rd", 4: "4th", 5: "5th"}
 
 
 def input_min_number():
-    min_number = input("Please input a minimum number: \n")
-    return int(min_number) if check_int_or_not(min_number) else input_min_number()
+    min_number = input("Please input a minimum number between 1 and 100: \n")
+    if check_int_or_not(min_number) is False:
+        return input_min_number()
+    min_number = int(min_number)
+    if check_range_min_and_max_number(min_number) is False:
+        return input_min_number()
+    return min_number
 
 
 def input_max_number(min_number: int):
-    max_number = input("Please input a maximum number: \n")
-    if check_int_or_not(max_number):
-        max_number = int(max_number)
-        if max_number > min_number:
-            return max_number
-        else:
-            print("The maximum number must be greater than the minimum number.")
-            return input_max_number(min_number)
-    else:
+    max_number = input("Please input a maximum number between 1 and 100: \n")
+    if check_int_or_not(max_number) is False:
         return input_max_number(min_number)
+    max_number = int(max_number)
+    if check_valid_max_number(min_number, max_number) is False:
+        return input_max_number(min_number)
+    if check_range_min_and_max_number(max_number) is False:
+        return input_max_number(min_number)
+    return max_number
 
 
-def input_min_and_max():
+def main():
     min_number = input_min_number()
     max_number = input_max_number(min_number)
     ans_number = random.randint(min_number, max_number)
@@ -51,5 +55,21 @@ def check_int_or_not(input: str):
         return False
 
 
+def check_range_min_and_max_number(num: int):
+    if 1 <= num <= 100:
+        return True
+    else:
+        print("Invalid range. Please input between 1 and 100.")
+        return False
+
+
+def check_valid_max_number(min_num: int, max_num: int):
+    if max_num > min_num:
+        return True
+    else:
+        print("The maximum number must be greater than the minimum number.")
+        return False
+
+
 if __name__ == "__main__":
-    input_min_and_max()
+    main()
